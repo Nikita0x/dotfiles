6a01c4f2a298a41682350ffebc1acdec7f7f8a95
Nikita0x <klirmio21@gmail.com>
2024-01-26 17:59:45 +0200

    updated

--- setup.sh
+++ setup.sh
@@ -21,6 +21,24 @@ curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
 # Install python
 sudo apt install python3
 
+# Install python virtual environment
+sudo apt install python3.10-venv
+
+# Install pip (package manager for python)
+sudo apt install python3-pip
+
+# Install django framework
+sudo apt install python3-django
+
+# Handly command to visualize the structure of the folder
+sudo apt install tree
+
+# Package to see and kill processes on Linux
+sudo apt install htop
+
+# SQLite browser
+sudo apt-get install sqlitebrowser
+
 echo "********************************************************"
 echo "Setup complete! Close and reopen the terminal to take effect."
 echo "********************************************************"
