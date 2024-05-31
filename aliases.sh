51b4346733c2b52f40ce87790182588d1d43b780
Nikita0x <klirmio21@gmail.com>
2024-05-31 10:45:45 +0300

    добавлен новый алиас - "sysmon"

--- aliases.sh
+++ aliases.sh
@@ -1,3 +1,4 @@
 echo "alias studio='cd /opt/android-studio/bin && ./studio.sh'" >> ~/.bashrc
+echo "alias sysmon='gnome-system-monitor'" >> ~/.bashrc
 # Inform the user to source .bashrc
 echo "Please run 'source ~/.bashrc' to apply the changes or open a new terminal."
