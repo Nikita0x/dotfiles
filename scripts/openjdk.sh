4163c62d9ecb4f816eab5dd3ea4441ad6257efe1
Nikita0x <klirmio21@gmail.com>
2025-05-06 16:59:22 +0300

    added: openjdk-21 install - and some comments on how to switch it

--- scripts/openjdk.sh
+++ scripts/openjdk.sh
@@ -6,3 +6,16 @@ echo
 
 sudo apt update
 sudo apt install openjdk-17-jdk -y
+
+echo "********************************************************"
+echo Installing openjdk-21-jdk
+echo "********************************************************"
+echo 
+
+sudo apt install openjdk-21-jdk -y
+
+# javac --version
+# java --verision
+
+# sudo update-alternatives --config java    -to change version
+# sudo update-alternatives --config javac   -to change version
