d18298a40fd3731db485ecd0446cf09ff5d34d1b
Nikita0x <klirmio21@gmail.com>
2025-10-20 10:28:57 +0300

    added: xremap for keybouard remapping

--- scripts/cargo.sh
+++ scripts/cargo.sh
@@ -3,4 +3,11 @@ echo Installing watchexec
 echo "********************************************************"
 echo 
 
-cargo install watchexec-cli
+
+
+echo "********************************************************"
+echo Installing xremap
+echo "********************************************************"
+echo 
+
+cargo install xremap
