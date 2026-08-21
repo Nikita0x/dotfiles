b684a4e9efcd56b0f22a15f259440ae101e01068
Nikita0x <klirmio21@gmail.com>
2026-08-21 16:38:28 +0300

    removed telegram from flathub - now installing it from snap

--- scripts/flathub.sh
+++ scripts/flathub.sh
@@ -1,13 +1,6 @@
 echo "********************************************************"
 echo Installing bottles
 echo "********************************************************"
-echo 
+echo
 
 flatpak install flathub com.usebottles.bottles -y
-
-echo "********************************************************"
-echo Installing telegram
-echo "********************************************************"
-echo 
-
-flatpak install flathub org.telegram.desktop -y
