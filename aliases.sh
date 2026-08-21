64205ec2d13497236257665522a26d3b7f20661f
Nikita0x <klirmio21@gmail.com>
2026-08-21 17:33:12 +0300

    update keymaps for zed

--- aliases.sh
+++ aliases.sh
@@ -22,5 +22,8 @@ add_alias_if_missing "alias св='cd'"
 add_alias_if_missing "alias dotfiles='zed ~/github/dotfiles'"
 add_alias_if_missing "alias lock='cinnamon-screensaver-command --lock'"
 
+# Line below enables ctrl+backspace - to delete whole word in terminal (without it only alt+backspace works)
+echo "stty werase '^H'" >> ~/.bashrc
+
 echo
 echo "Please run 'source ~/.bashrc' to apply the changes or open a new terminal."
